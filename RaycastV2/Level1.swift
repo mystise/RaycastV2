6691f383f5dd645b16ece4a19e5259bee00efff0
Isaac Dudney <isaac.dudney@icloud.com>
2015-07-12 10:46:52 -0700

    Complete (but probably error'd) level

--- RaycastV2/Level1.swift
+++ RaycastV2/Level1.swift
@@ -10,24 +10,33 @@ import Foundation
 
 func level1() -> Level {
     //var level = Level(size: IRect(point1: IVector(x: 0, y: 0), point2: IVector(x: 100, y: 75)), walls:[], enemies:[], spawn: IVector(x: 2, y: 2), exit: IRect(point1: IVector(x: 45, y: 45), point2: IVector(x: 50, y: 50)))
-    var level = Level(size: ISize(width: 100, height: 100), walls:[], enemies:[], spawn: IVector(x: 2, y: 2), exit: IRect(point1: IVector(x: 45, y: 45), point2: IVector(x: 50, y: 50)))
+    var level = Level(size: ISize(width: 100, height: 100), walls:[], enemies:[], spawn: IVector(x: 2, y: 2), exit: IRect(point1: IVector(x: 30, y: 45), point2: IVector(x: 35, y: 50)))
     
     level.walls = [ILineSeg(point1: IVector(x: 5, y: 0), point2: IVector(x: 5, y: 10)),
-                   ILineSeg(point1: IVector(x: 5, y: 10), point2: IVector(x: 25, y: 10)),
-                   ILineSeg(point1: IVector(x: 5, y: 15), point2: IVector(x: 25, y: 15)),
-                   ILineSeg(point1: IVector(x: 25, y: 10), point2: IVector(x: 25, y: 15)),
+                   ILineSeg(point1: IVector(x: 5, y: 10), point2: IVector(x: 15, y: 10)),
+                   ILineSeg(point1: IVector(x: 5, y: 15), point2: IVector(x: 15, y: 15)),
+                   ILineSeg(point1: IVector(x: 15, y: 10), point2: IVector(x: 15, y: 15)),
                    ILineSeg(point1: IVector(x: 7, y: 0), point2: IVector(x: 7, y: 25)),
                    ILineSeg(point1: IVector(x: 5, y: 15), point2: IVector(x: 5, y: 25)),
                    ILineSeg(point1: IVector(x: 5, y: 25), point2: IVector(x: 7, y: 25)),
                    ILineSeg(point1: IVector(x: 0, y: 30), point2: IVector(x: 30, y: 30)),
                    ILineSeg(point1: IVector(x: 30, y: 30), point2: IVector(x: 30, y: 10)),
-                   ILineSeg(point1: IVector(x: 35, y: 7), point2: IVector(x: 25, y: 7)),
+                   ILineSeg(point1: IVector(x: 35, y: 10), point2: IVector(x: 25, y: 10)),
                    ILineSeg(point1: IVector(x: 35, y: 10), point2: IVector(x: 40, y: 15)),
-                   ILineSeg(point1: IVector(x: 32, y: 0), point2: IVector(x: 45, y: 15)),
+                   ILineSeg(point1: IVector(x: 30, y: 0), point2: IVector(x: 45, y: 15)),
                    ILineSeg(point1: IVector(x: 40, y: 15), point2: IVector(x: 35, y: 20)),
                    ILineSeg(point1: IVector(x: 45, y: 15), point2: IVector(x: 40, y: 20)),
                    ILineSeg(point1: IVector(x: 35, y: 20), point2: IVector(x: 40, y: 25)),
-                   ILineSeg(point1: IVector(x: 40, y: 20), point2: IVector(x: 45, y: 25))]
+                   ILineSeg(point1: IVector(x: 40, y: 20), point2: IVector(x: 45, y: 25)),
+                   ILineSeg(point1: IVector(x: 40, y: 25), point2: IVector(x: 35, y: 30)),
+                   ILineSeg(point1: IVector(x: 40, y: 20), point2: IVector(x: 30, y: 30)),
+                   ILineSeg(point1: IVector(x: 35, y: 30), point2: IVector(x: 40, y: 35)),
+                   ILineSeg(point1: IVector(x: 30, y: 30), point2: IVector(x: 35, y: 35)),
+                   ILineSeg(point1: IVector(x: 40, y: 35), point2: IVector(x: 50, y: 35)),
+                   ILineSeg(point1: IVector(x: 35, y: 35), point2: IVector(x: 25, y: 35)),
+                   ILineSeg(point1: IVector(x: 25, y: 35), point2: IVector(x: 25, y: 50)),
+                   ILineSeg(point1: IVector(x: 50, y: 35), point2: IVector(x: 50, y: 50)),
+                   ILineSeg(point1: IVector(x: 50, y: 50), point2: IVector(x: 25, y: 50))]
     
     let waypoints: [Waypoint] = [Waypoint(pos: IVector(x: 5, y: 20),
                                  speed: 1,
